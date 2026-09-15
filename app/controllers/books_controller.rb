427092f120b75eb944756c5f5ecf9bbaf0c3bdb9
Tayte Choudhury <195010562+t8ctamu@users.noreply.github.com>
2026-09-14 20:19:04 -0500

    Complete book details forms and passing RSpec coverage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,6 +49,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :author, :price, :published_date)
   end
 end
